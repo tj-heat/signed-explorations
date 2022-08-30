8dc918b56c92216dd5c1ae8d199e6e72fc76755f
Treff <elevator.cakes@gmail.com>
2022-08-30 14:11:35 +1000

    Top down physics/user movement implemented

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ from typing import Optional
 from arcade.pymunk_physics_engine import PymunkPhysicsEngine
 
 SPRITE_SCALING = 0.25
-MOVEMENT_SPEED = 5
+MOVEMENT_SPEED = 3
 SPRITE_IMAGE_SIZE = 250
 SPRITE_SIZE = int(SPRITE_IMAGE_SIZE * SPRITE_SCALING)
 
@@ -22,18 +22,18 @@ class GameView(arcade.View):
         super().__init__()
         arcade.set_background_color(arcade.color.CORNFLOWER_BLUE)
 
-        #self.player_list = None
-        #self.wall_list = None
-        self.player_sprite = None
-
         #Our scene object
         self.scene = None
+        #player object
+        self.player_sprite = None
+        #phys engine
+        self.physics_engine = None
 
-        # Track the current state of what key is pressed
         self.left_pressed = False
         self.right_pressed = False
         self.up_pressed = False
         self.down_pressed = False
+
     
     def setup(self):
         #Initialise scene
@@ -52,34 +52,71 @@ class GameView(arcade.View):
             wall.center_x = x
             wall.center_y = SPRITE_SIZE/2
             self.scene.add_sprite("Walls", wall)
+        
+        #Create physics engine
+        """self.physics_engine = arcade.PhysicsEngineSimple(
+            self.player_sprite, self.scene.get_sprite_list("Walls")
+        )"""
+
+        self.physics_engine = PymunkPhysicsEngine(damping=0.7, gravity=(0,0))
+        self.physics_engine.add_sprite_list(self.scene.get_sprite_list("Player"),
+            friction = 0.6,
+            moment_of_intertia=PymunkPhysicsEngine.MOMENT_INF,
+            damping = 0.01,
+            collision_type="player")
+
+        self.physics_engine.add_sprite_list(self.scene.get_sprite_list("Walls"),
+            friction = 0.6,
+            collision_type="wall",
+            body_type = PymunkPhysicsEngine.STATIC)
+        
 
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
 
-        if key == arcade.key.UP:
+        if key == arcade.key.UP or key == arcade.key.W:
             self.up_pressed = True
-        elif key == arcade.key.DOWN:
+        elif key == arcade.key.DOWN or key == arcade.key.S:
             self.down_pressed = True
-        elif key == arcade.key.LEFT:
+        elif key == arcade.key.LEFT or key == arcade.key.A:
             self.left_pressed = True
-        elif key == arcade.key.RIGHT:
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.right_pressed = True
 
     def on_key_release(self, key, modifiers):
         """Called when the user releases a key. """
 
-        if key == arcade.key.UP:
+        if key == arcade.key.UP or key == arcade.key.W:
             self.up_pressed = False
-        elif key == arcade.key.DOWN:
+        elif key == arcade.key.DOWN or key == arcade.key.S:
             self.down_pressed = False
-        elif key == arcade.key.LEFT:
+        elif key == arcade.key.LEFT or key == arcade.key.A:
             self.left_pressed = False
-        elif key == arcade.key.RIGHT:
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.right_pressed = False
 
     def on_update(self, delta_time):
         """ Movement and game logic """
+        self.player_sprite.change_x = 0
+        self.player_sprite.change_y = 0
+
+        if self.up_pressed and not self.down_pressed:
+            force = (0, PLAYER_MOVE_FORCE)
+            self.physics_engine.apply_force(self.player_sprite, force)
+        elif self.down_pressed and not self.up_pressed:
+            force = (0, -PLAYER_MOVE_FORCE)
+            self.physics_engine.apply_force(self.player_sprite, force)
+        if self.left_pressed and not self.right_pressed:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+            force = (-PLAYER_MOVE_FORCE, 0)
+            self.physics_engine.apply_force(self.player_sprite, force)
+        elif self.right_pressed and not self.left_pressed:
+            force = (PLAYER_MOVE_FORCE, 0)
+            self.physics_engine.apply_force(self.player_sprite, force)
+
+        # --- Move items in the physics engine
+        self.physics_engine.step()
 
 
     def on_draw(self):
