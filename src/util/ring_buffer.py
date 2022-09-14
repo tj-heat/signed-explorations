4500d3fd3404a8aead18efb05715d8fd05339585
E <e.roderick@uqconnect.edu.au>
2022-09-15 07:38:08 +1000

    Simplified ring buffer

--- src/util/ring_buffer.py
+++ src/util/ring_buffer.py
@@ -1,110 +1,93 @@
-from threading import Semaphore
-from typing import Any
+import queue
 
-class RingBuffer():
-    def __init__(self, capacity: int = 10, overlap: bool = False) -> None:
-        self._max_items = capacity
-        self._overlap = overlap
+class Closed(Exception):
+    """ Exception raised by RingBuffer.put/get when RingBuffer closed. """
+    pass
 
-        # Initial data
-        self._buffer = [None] * self._max_items
-        self._size = 0
-        self._head = 0
-        self._tail = -1 # Staggered initial value for proper pointing
+class NotifyBufferFinish():
+    """ This class is a sentinel to inform buffer consumers to stop buffer 
+    operations.
+    """
 
-        # Thread safety
-        self.__buffer_sem = Semaphore() # Used for operations involving buffer
-        self.__meta_sem = Semaphore() # Used for meta properties
-
-    def _get_next_value(self, value: int) -> int:
-        """ TODO """
-        return (value + 1) % self._max_items
-
-    def _move_head(self):
-        """ TODO """
-        self._head = self._get_next_value(self._head)
-
-    def _move_tail(self):
-        """ TODO """
-        self._tail = self._get_next_value(self._tail)
-
-    def _put(self, item: Any):
-        """ TODO """
-        if self.is_full():
-            if not self._overlap:
-                raise AttributeError("Cannot place item. Buffer is full.")
-
-            # Replace oldest item to allow for new
-            self._get()
-
-        # Add the item
-        self._buffer[self._head] = item
-        self._size += 1
-
-        # Move head to new position
-        self._move_head()
-
-    def _get(self) -> Any:
-        """ TODO """
-        if self.is_empty():
-            raise AttributeError("Cannot retrieve item. Buffer is empty.")
+class RingBuffer(queue.Queue):
+    """ A FIFO Queue implementation that allows for cyclic value replacement. 
+    """
+    def __init__(self, capacity: int = 10, overlap: bool = True) -> None:
+        """ Construct a RingBuffer instance.
         
-        # Move tail to new position
-        self._move_tail()
+        Params:
+            capacity (int): The maximum number of items the buffer can hold. 
+                Defaults to 0. capacity <= 0 means infinite.
+            overlap (bool): Whether the buffer placement can wrap. When false,
+                acts as a FIFO Queue. When true, acts as a ring buffer. Defaults
+                to True.
+        """
+        super().__init__(maxsize=capacity)
+        self._overlap = overlap
+        self._closed = False
 
-        # Retrieve oldest item
-        item = self._buffer[self._tail]
+    def _close(self):
+        """ Close the buffer so no more values can be placed/removed. """
+        self._closed = True
 
-        # Remove old item
-        self._buffer[self._tail] = None
-        self._size -= 1
+    def put(self, item, block=True, timeout=None):
+        """ Put an item onto the ring buffer.
+        
+        If the buffer allows overlap and is full, the oldest value in the buffer
+        will be replaced. If the buffer does not allow overlap, acts like a FIFO
+        queue.
 
-        return item
+        If the buffer is closed, raises Closed exception.            
 
-    def put(self, item: Any):
-        """ TODO """
-        with self.__buffer_sem:
-            self._put(item)
+        If optional args 'block' is true and 'timeout' is None (the default),
+        block if necessary until a free slot is available. If 'timeout' is
+        a non-negative number, it blocks at most 'timeout' seconds and raises
+        the Full exception if no free slot was available within that time.
+        Otherwise ('block' is false), put an item on the queue if a free slot
+        is immediately available, else raise the Full exception ('timeout'
+        is ignored in that case).
+        """
+        if self._closed:
+            raise Closed("The RingBuffer is closed. Cannot Put item")
 
-    def get(self) -> Any:
-        """ TODO """
-        with self.__buffer_sem:
-            item = self._get()
-            return item
+        if self._overlap and self.full():
+            self.get()
 
-    def peek(self) -> Any:
-        """ TODO """
-        with self.__buffer_sem:
-            if self.is_empty():
-                raise AttributeError("Cannot retrieve item. Buffer is empty.")
-            
-            item = self._buffer[self._get_next_value(self._tail)]
-            return item
+        super().put(item, block, timeout)
+        
+    def get(self, block=True, timeout=None):
+        """ Remove and return an item from the queue.
 
-    def get_capacity(self):
-        """ TODO """
-        return self._max_items
+        If the buffer is closed, raises Closed exception.
 
-    def get_size(self):
-        """ TODO """
-        with self.__meta_sem:
-            size = self._size
-            return size
+        If optional args 'block' is true and 'timeout' is None (the default),
+        block if necessary until an item is available. If 'timeout' is
+        a non-negative number, it blocks at most 'timeout' seconds and raises
+        the Empty exception if no item was available within that time.
+        Otherwise ('block' is false), return an item if one is immediately
+        available, else raise the Empty exception ('timeout' is ignored
+        in that case).
+        """
+        if self._closed:
+            raise Closed("The RingBuffer is closed. Cannot Put item")
+        return super().get(block, timeout)
 
-    def is_empty(self):
-        """ TODO """
-        return self.get_size() == 0
+    def capacity(self):
+        return self.maxsize
 
-    def is_full(self):
-        """ TODO """
-        return self.get_size() == self.get_capacity()
+    def notify_finish(self):
+        """ Begin the process of notifying all consumers that the buffer should
+        close.
+        """
+        self.put(NotifyBufferFinish())
 
-    def __str__(self) -> str:
-        with self.__meta_sem:
-            result =  f"Head: {self._head} Tail: {self._tail}\nData: {self._buffer}"
-
-            return result
-
-
-
-
+    def confirm_sentinel(self, sentinel: NotifyBufferFinish) -> None:
+        """ Used to acknowledge that a sentinel has been received. Will 
+        propagate the sentinel to other consumers, unless the buffer is empty.
+        If the buffer is empty, the buffer will be closed.
+        """
+        if self.empty():
+            self._close()
+        else:
+            self.put(sentinel)
+        
