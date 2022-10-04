0668feedebfa7b4a76c3d97b52a559f0cb9d3b6e
E <e.roderick@uqconnect.edu.au>
2022-10-04 13:14:34 +1000

    Made event trigger correct size/colour

--- src/actors/event_triggers.py
+++ src/actors/event_triggers.py
@@ -2,20 +2,12 @@ import arcade
 
 TEST_PATH = "assets/tilemaps/tutorial/textures/testing.png"
 
-class EventTrigger(arcade.Sprite):
-    # def __init__(self, width, height, task) -> None:
-    #     super().__init__()
-
-    #     self.type = "Event"
-    #     self.texture = arcade.load_texture(TEST_PATH)
-    #     self.hit_box = self.texture.hit_box_points
-    
+class EventTrigger(arcade.Sprite):   
     def __init__(self, width, height, task) -> None:
-        texture = arcade.Texture.create_empty("EMPTY", [64, 64])
-        super().__init__(image_width=width, image_height=height)
+        texture = arcade.Texture.create_empty("EMPTY", [width, height])
+        super().__init__(image_width=width, image_height=height, texture=None)
         self.hit_box = texture.hit_box_points
-        print(self.hit_box)
 
-        self._width = width
-        self._height = height
+        print(self.hit_box)
+        self.draw_hit_box()
         self._task = task
