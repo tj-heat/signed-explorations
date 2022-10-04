15a42d68b8748670996eb3e4c85021f9d495bb3b
E <e.roderick@uqconnect.edu.au>
2022-10-04 16:06:38 +1000

    Cleaned event trigger class

--- src/actors/event_triggers.py
+++ src/actors/event_triggers.py
@@ -1,13 +1,33 @@
+from typing import Tuple
+
 import arcade
 
 TEST_PATH = "assets/tilemaps/tutorial/textures/testing.png"
 
 class EventTrigger(arcade.Sprite):   
-    def __init__(self, width, height, task) -> None:
-        texture = arcade.Texture.create_empty("EMPTY", [width, height])
-        super().__init__(image_width=width, image_height=height, texture=None)
-        self.hit_box = texture.hit_box_points
+    def __init__(self, width, height, task, debug = True) -> None:
+        # Construct sprite
+        super().__init__(
+            image_width=width, 
+            image_height=height, 
+            texture=None
+        )
+        if debug:
+            texture = arcade.Texture.create_filled(
+                f"FILLED_EVENT_{self.guid}", 
+                [width, height], 
+                color=arcade.color.BLUE
+            )
+            self.texture = texture
 
-        print(self.hit_box)
-        self.draw_hit_box()
+        self.hit_box = EventTrigger.calc_relative_bbox(width, height)
         self._task = task
+
+    @staticmethod
+    def calc_relative_bbox(width, height) -> Tuple["Point"]:
+        """ Calculates the points needed for a relative bounding box, based on 
+        width and height.
+        """
+        x, y, = (width / 2, height / 2)
+
+        return ((-x, -y), (x, -y), (x, y), (-x, y))
