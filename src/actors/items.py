af2876117842baebc1051b11b3fef5ce5a8c7a55
Treff <elevator.cakes@gmail.com>
2022-10-12 14:58:51 +1000

    Side doors spawning properly

--- src/actors/items.py
+++ src/actors/items.py
@@ -22,16 +22,17 @@ class Door(arcade.Sprite):
         self.task = Task.DOOR
         self.key = key
 
-        self.texture = arcade.load_texture(DOOR_PATH + ".png")
+        self.open_texture_pair = None
+        if option == "side" and orientation == "left":
+            self.open_texture_pair = [arcade.load_texture(f"{DOOR_PATH}_side.png"), arcade.load_texture(f"{DOOR_PATH}_side_open.png")]
+        elif option == "side" and orientation == "right":
+            self.open_texture_pair = [arcade.load_texture(f"{DOOR_PATH}_side.png", flipped_horizontally=True), arcade.load_texture(f"{DOOR_PATH}_side_open.png", flipped_horizontally=True)]
+        else:
+            self.open_texture_pair = [arcade.load_texture(f"{DOOR_PATH}.png"), arcade.load_texture(f"{DOOR_PATH}_open.png")]
+
+        self.texture = self.open_texture_pair[0]
         self.hit_box = self.texture.hit_box_points
 
-        # self.open_texture_pair = None
-        # if option == "side" and orientation == "left":
-        #     self.open_texture_pair = [arcade.load_texture(f"{DOOR_PATH}_side.png"), arcade.load_texture(f"{DOOR_PATH}_side_open.png")]
-        # elif option == "side" and orientation == "right":
-        #     self.open_texture_pair = [arcade.load_texture(f"{DOOR_PATH}_side.png", flipped_horizontally=True), arcade.load_texture(f"{DOOR_PATH}_side_open.png", flipped_horizontally=True)]
-        # else:
-        #     self.open_texture_pair = [arcade.load_texture(f"{DOOR_PATH}.png"), arcade.load_texture(f"{DOOR_PATH}_open.png")]
 
     def open_door(self):
         self.texture = self.open_texture_pair[1]
