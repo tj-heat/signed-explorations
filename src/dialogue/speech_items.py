954e30ba50de4fe159034ed11ae3aea6192215cd
E <e.roderick@uqconnect.edu.au>
2022-10-13 10:16:36 +1000

    Added place for dialogue constants

---
+++ src/dialogue/speech_items.py
@@ -0,0 +1,24 @@
+DIALOGUE_INTRODUCTION = [
+    "Before you stands a dog.",
+    "He is happy to see you.",
+    "In his hands is a keyboard. (Where did he get that from?)",
+    "You can use <W> <A> <S> <D> to move your character.",
+    "<E> is used to interact with the world around you.",
+    "<Q> can also call the dog to your side.",
+]
+
+KEY_FIRST = [
+    "I left my keys in my other body.",
+]
+
+BRIDGE_MIDDLE = [
+    "L'appel du vide...",
+]
+
+PUZZLE_INTERACT = [
+    "Your paw passes straight through.",
+]
+
+MARBLE_FLOOR = [
+    "This marble feels cold... I think?",
+]
