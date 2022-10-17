efa0bf33b6f8824101193ad793e4384724ae36a2
josephjmet <josephjmet@gmail.com>
2022-10-17 19:07:03 +1000

    finalised book_view

--- src/views/Book_view.py
+++ src/views/Book_view.py
@@ -24,37 +24,69 @@ class BookView(arcade.View):
         self.v_box.add(green_button)
         self.v_box.add(red_button)
 
-        a_button = arcade.gui.UITextureButton(x=134, y=524, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Red.png'))   
-        b_button = arcade.gui.UITextureButton(x=234, y=524, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Green.png'))
-        c_button = arcade.gui.UITextureButton(x=334, y=524, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Red.png')) 
+        a_label = arcade.gui.UILabel(x=134, y=524, width=36, height=50, text="A", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)   
+        b_label = arcade.gui.UILabel(x=234, y=524, width=36, height=50, text="B", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)
+        c_label = arcade.gui.UILabel(x=334, y=524, width=36, height=50, text="C", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36) 
 
-        d_button = arcade.gui.UITextureButton(x=134, y=464, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Green.png'))
-        e_button = arcade.gui.UITextureButton(x=234, y=464, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Red.png'))   
-        f_button = arcade.gui.UITextureButton(x=334, y=464, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Green.png'))
+        d_label = arcade.gui.UILabel(x=134, y=464, width=36, height=50, text="D", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)
+        e_label = arcade.gui.UILabel(x=234, y=464, width=36, height=50, text="E", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)   
+        f_label = arcade.gui.UILabel(x=334, y=464, width=36, height=50, text="F", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)
 
-        g_button = arcade.gui.UITextureButton(x=134, y=404, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Red.png'))   
-        i_button = arcade.gui.UITextureButton(x=234, y=404, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Green.png'))
-        k_button = arcade.gui.UITextureButton(x=334, y=404, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Red.png')) 
+        g_label = arcade.gui.UILabel(x=134, y=404, width=36, height=50, text="G", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)   
+        i_label = arcade.gui.UILabel(x=234, y=404, width=36, height=50, text="I", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)
+        k_label = arcade.gui.UILabel(x=334, y=404, width=36, height=50, text="K", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36) 
 
-        l_button = arcade.gui.UITextureButton(x=134, y=344, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Green.png'))
-        m_button = arcade.gui.UITextureButton(x=234, y=344, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Red.png'))   
-        n_button = arcade.gui.UITextureButton(x=334, y=344, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Green.png'))
+        l_label = arcade.gui.UILabel(x=134, y=344, width=36, height=50, text="L", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)
+        m_label = arcade.gui.UILabel(x=234, y=344, width=36, height=50, text="M", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)   
+        n_label = arcade.gui.UILabel(x=334, y=344, width=36, height=50, text="N", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)
 
-        o_button = arcade.gui.UITextureButton(x=134, y=284, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Red.png'))   
-        p_button = arcade.gui.UITextureButton(x=234, y=284, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Green.png'))
-        q_button = arcade.gui.UITextureButton(x=334, y=284, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Red.png')) 
+        o_label = arcade.gui.UILabel(x=134, y=284, width=36, height=50, text="O", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)   
+        p_label = arcade.gui.UILabel(x=234, y=284, width=36, height=50, text="P", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)
+        q_label = arcade.gui.UILabel(x=334, y=284, width=36, height=50, text="Q", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36) 
 
-        r_button = arcade.gui.UITextureButton(x=134, y=224, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Green.png'))
-        s_button = arcade.gui.UITextureButton(x=234, y=224, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Red.png'))   
-        t_button = arcade.gui.UITextureButton(x=334, y=224, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Green.png'))
+        r_label = arcade.gui.UILabel(x=134, y=224, width=36, height=50, text="R", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)
+        s_label = arcade.gui.UILabel(x=234, y=224, width=36, height=50, text="S", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)   
+        t_label = arcade.gui.UILabel(x=334, y=224, width=36, height=50, text="T", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)
 
-        u_button = arcade.gui.UITextureButton(x=134, y=164, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Red.png'))   
-        v_button = arcade.gui.UITextureButton(x=234, y=164, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Green.png'))
-        w_button = arcade.gui.UITextureButton(x=334, y=164, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Red.png')) 
+        u_label = arcade.gui.UILabel(x=134, y=164, width=36, height=50, text="U", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)   
+        v_label = arcade.gui.UILabel(x=234, y=164, width=36, height=50, text="V", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)
+        w_label = arcade.gui.UILabel(x=334, y=164, width=36, height=50, text="W", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36) 
 
-        x_button = arcade.gui.UITextureButton(x=134, y=104, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Green.png'))
-        y_button = arcade.gui.UITextureButton(x=234, y=104, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Red.png'))   
-        z_button = arcade.gui.UITextureButton(x=334, y=104, width=36, height=50, texture=arcade.load_texture('assets\interface\Book_UI_Tabs_Green.png'))
+        x_label = arcade.gui.UILabel(x=134, y=104, width=36, height=50, text="X", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)
+        y_label = arcade.gui.UILabel(x=234, y=104, width=36, height=50, text="Y", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)   
+        z_label = arcade.gui.UILabel(x=334, y=104, width=36, height=50, text="Z", text_color=(0,0,0), font_name="Kenney Mini Square", font_size= 36)
+
+        a_button = arcade.gui.UIInteractiveWidget(x=134, y=524, width=36, height=50)   
+        b_button = arcade.gui.UIInteractiveWidget(x=234, y=524, width=36, height=50)
+        c_button = arcade.gui.UIInteractiveWidget(x=334, y=524, width=36, height=50) 
+
+        d_button = arcade.gui.UIInteractiveWidget(x=134, y=464, width=36, height=50)
+        e_button = arcade.gui.UIInteractiveWidget(x=234, y=464, width=36, height=50)   
+        f_button = arcade.gui.UIInteractiveWidget(x=334, y=464, width=36, height=50)
+
+        g_button = arcade.gui.UIInteractiveWidget(x=134, y=404, width=36, height=50)   
+        i_button = arcade.gui.UIInteractiveWidget(x=234, y=404, width=36, height=50)
+        k_button = arcade.gui.UIInteractiveWidget(x=334, y=404, width=36, height=50) 
+
+        l_button = arcade.gui.UIInteractiveWidget(x=134, y=344, width=36, height=50)
+        m_button = arcade.gui.UIInteractiveWidget(x=234, y=344, width=36, height=50)   
+        n_button = arcade.gui.UIInteractiveWidget(x=334, y=344, width=36, height=50)
+
+        o_button = arcade.gui.UIInteractiveWidget(x=134, y=284, width=36, height=50)   
+        p_button = arcade.gui.UIInteractiveWidget(x=234, y=284, width=36, height=50)
+        q_button = arcade.gui.UIInteractiveWidget(x=334, y=284, width=36, height=50) 
+
+        r_button = arcade.gui.UIInteractiveWidget(x=134, y=224, width=36, height=50)
+        s_button = arcade.gui.UIInteractiveWidget(x=234, y=224, width=36, height=50)   
+        t_button = arcade.gui.UIInteractiveWidget(x=334, y=224, width=36, height=50)
+
+        u_button = arcade.gui.UIInteractiveWidget(x=134, y=164, width=36, height=50)   
+        v_button = arcade.gui.UIInteractiveWidget(x=234, y=164, width=36, height=50)
+        w_button = arcade.gui.UIInteractiveWidget(x=334, y=164, width=36, height=50) 
+
+        x_button = arcade.gui.UIInteractiveWidget(x=134, y=104, width=36, height=50)
+        y_button = arcade.gui.UIInteractiveWidget(x=234, y=104, width=36, height=50)   
+        z_button = arcade.gui.UIInteractiveWidget(x=334, y=104, width=36, height=50)
 
         self.v_box.add(a_button)
         self.v_box.add(b_button)
@@ -80,6 +112,30 @@ class BookView(arcade.View):
         self.v_box.add(x_button)
         self.v_box.add(y_button)
         self.v_box.add(z_button)
+        self.v_box.add(a_label)
+        self.v_box.add(b_label)
+        self.v_box.add(c_label)
+        self.v_box.add(d_label)
+        self.v_box.add(e_label)
+        self.v_box.add(f_label)
+        self.v_box.add(g_label)
+        self.v_box.add(i_label)
+        self.v_box.add(k_label)
+        self.v_box.add(l_label)
+        self.v_box.add(m_label)
+        self.v_box.add(n_label)
+        self.v_box.add(o_label)
+        self.v_box.add(p_label)
+        self.v_box.add(q_label)
+        self.v_box.add(r_label)
+        self.v_box.add(s_label)
+        self.v_box.add(t_label)
+        self.v_box.add(u_label)
+        self.v_box.add(v_label)
+        self.v_box.add(w_label)
+        self.v_box.add(x_label)
+        self.v_box.add(y_label)
+        self.v_box.add(z_label)
 
         a_button.on_click = lambda e: self.on_click_letter_button(letter= "A")
         b_button.on_click = lambda e: self.on_click_letter_button(letter= "B")
@@ -124,7 +180,7 @@ class BookView(arcade.View):
         if self.front_image is not None:
             arcade.draw_texture_rectangle(730, 475, 340, 240, self.front_image)
         if self.back_image is not None:
-            arcade.draw_texture_rectangle(560, 120, 340, 240, self.back_image)
+            arcade.draw_texture_rectangle(730, 225, 340, 240, self.back_image)
         self.manager.draw()
     
     def on_update(self, delta_time):
@@ -141,7 +197,11 @@ class BookView(arcade.View):
 
     def on_click_letter_button(self, letter):
         image_front = "assets\interface\\" + letter + "-FRONT-VIEW.PNG"
+        print(image_front)
         self.front_image = arcade.load_texture(image_front) 
-        #image_back = "assets\interface\\" + letter + "-BACK-VIEW.PNG"
-        #self.back_image = arcade.load_texture(image_back) 
+        if letter == "K" or letter == "E" or letter == "Y":
+            image_back = "assets\interface\\" + letter + "-BACK-VIEW.PNG"
+            self.back_image = arcade.load_texture(image_back) 
+        else:
+            self.back_image = None
 
