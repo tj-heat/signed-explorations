03cd03f65d8d2856340a7b191e89ed6f41a07d59
Airo1011 <airo1011@gmail.com>
2022-10-19 01:27:18 +1000

    New Map Added - Boudnaries need to be checked

---
+++ assets/tilemaps/texture_maps/cliff-pixilart(2).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="cliff-pixilart(2)" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+ <image source="D:/Users/Aditya/Downloads/cliff-pixilart(2).png" width="64" height="64"/>
+</tileset>
---
+++ assets/tilemaps/texture_maps/waterfall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="waterfall" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+ <image source="../../tiles/pixil-frame-0(5).png" width="64" height="64"/>
+</tileset>
