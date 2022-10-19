07ba4e10e06e834a8e386b42c61fc7fa9f7442d3
E <e.roderick@uqconnect.edu.au>
2022-10-19 13:44:44 +1000

    Fullscreen

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ SCREEN_TITLE = "Signed Explorations"
 LOGO_PATH = "assets/logos/TJHeat_L_C_250.png"
 
 def main():
-    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, resizable  = True) #fullscreen = True resizable = True
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, fullscreen=True) #fullscreen = True resizable = True
     window.set_icon(pyglet.resource.image(LOGO_PATH))
     start_view = StartView.StartView() #StartView() #game.GameView() 
     start_view.setup()
